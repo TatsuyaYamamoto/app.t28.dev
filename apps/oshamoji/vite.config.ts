86de673cef419cc8d420f78e73e93c23d9ce3294
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2024-01-05 12:29:13 +0900

    chore: set head

--- apps/oshamoji/vite.config.ts
+++ apps/oshamoji/vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 const basePath = "/oshamoji";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: basePath,
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: { ejsOptions: { views: ["../../shared/ejs"] } },
+    }),
+  ],
   build: {
     outDir: `../../dist${basePath}`,
   },
