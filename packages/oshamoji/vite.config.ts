103da088305d3916374b540fb64729a96d139a5b
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2024-08-29 21:04:58 +0900

    chore: fix build

--- packages/oshamoji/vite.config.ts
+++ packages/oshamoji/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [
     react(),
     createHtmlPlugin({
-      inject: { ejsOptions: { views: ["../../shared/ejs"] } },
+      inject: { ejsOptions: { views: ["../shared/ejs"] } },
     }),
   ],
   build: {
