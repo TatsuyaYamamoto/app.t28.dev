98c725e333b1b830a4e385fdce49cb09ad276c28
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2025-02-08 16:50:34 +0900

    chore: edit e2e config

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ["html"],
-    process.env.CI && ["json", { outputFile: "results.json" }],
+    ["json", { outputFile: "playwright-report/results.json" }],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
