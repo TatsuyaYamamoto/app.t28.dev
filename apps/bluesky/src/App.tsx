8bc0a571d9b970edb32013d29b2487df9ee2c4e4
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2025-02-12 15:10:58 +0900

    chore: add signin form

--- apps/bluesky/src/App.tsx
+++ apps/bluesky/src/App.tsx
@@ -1,12 +1,25 @@
-import PostView from "@/components/PostView/PostView.tsx";
 import { Box } from "@chakra-ui/react";
-import { FC } from "react";
+import { FC, useState } from "react";
+
+import PostView from "@/components/PostView/PostView.tsx";
+import SignInForm, { type SignInInputs } from "@/components/SignInForm.tsx";
 
 const App: FC = () => {
+  const [shouldShowSignIn] = useState(true);
+
+  const onRequestSingIn = async (inputs: SignInInputs) => {
+    console.log(inputs);
+    return { isSuccess: true };
+  };
+
   return (
     <>
       <Box as="main" display="flex" justifyContent="center" height="100%">
-        <PostView />
+        {shouldShowSignIn ? (
+          <SignInForm onRequestSingIn={onRequestSingIn} />
+        ) : (
+          <PostView />
+        )}
       </Box>
     </>
   );
