d7be2a8eb21528191a2297e40d2fc8ca485b7305
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2025-02-16 01:39:33 +0900

    chore: setup build

--- apps/bluesky/vite.config.ts
+++ apps/bluesky/vite.config.ts
@@ -1,8 +1,21 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
 import tsconfigPaths from "vite-tsconfig-paths";
+
+const basePath = "/bluesky";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  base: basePath,
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    createHtmlPlugin({
+      inject: { ejsOptions: { views: ["../../packages/shared/ejs"] } },
+    }),
+  ],
+  build: {
+    outDir: `../../dist${basePath}`,
+  },
 });
