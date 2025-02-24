4ddeb06c4dcdaec59d09c672dc8e6e61375f5bf2
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2025-02-24 17:39:54 +0900

    refactor: form hook

--- apps/tweet-to-sky/src/App.tsx
+++ apps/tweet-to-sky/src/App.tsx
@@ -1,8 +1,8 @@
 import { AtUri } from "@atproto/api";
 import { Box, Flex } from "@chakra-ui/react";
-import { FC, useEffect } from "react";
-import { FormProvider, useForm } from "react-hook-form";
-import { enrichTweet, useTweet } from "react-tweet";
+import { FC } from "react";
+import { FormProvider } from "react-hook-form";
+import { useTweet } from "react-tweet";
 
 import { useAgent } from "@/components/AgentProvider.tsx";
 import Header from "@/components/Header.tsx";
@@ -11,18 +11,14 @@ import PostView, { PostForm } from "@/components/PostView/PostView.tsx";
 import SignInForm from "@/components/SignInForm.tsx";
 import { BORDER_COLOR } from "@/constants.ts";
 import { postToBluesky } from "@/helpers/bluesky.ts";
+import { usePostForm } from "@/hooks/usePostForm.ts";
 import { useTweetInUrl } from "@/hooks/useTweetInUrl.ts";
 
 const App: FC = () => {
   const { agent, isSessionAvailable } = useAgent();
   const [tweetId, clearTweetId] = useTweetInUrl();
   const { data: tweet } = useTweet(tweetId);
-  const postFormMethods = useForm<PostForm>({
-    defaultValues: {
-      text: "",
-      images: [],
-    },
-  });
+  const postFormMethods = usePostForm(tweet ?? undefined);
 
   const onPost = async (formValue: PostForm) => {
     const res = await postToBluesky(agent, formValue.text, {
@@ -39,40 +35,6 @@ const App: FC = () => {
       },
     });
   };
-
-  useEffect(() => {
-    if (!tweet) {
-      return;
-    }
-
-    const enrichedTweet = enrichTweet(tweet);
-    const text = enrichedTweet.entities.map(({ text }) => text).join("");
-
-    postFormMethods.setValue("text", text);
-
-    const imagesPromise = tweet.mediaDetails?.flatMap((media) => {
-      if (media.type !== "photo") {
-        return [];
-      }
-
-      return fetch(media.media_url_https)
-        .then((res) => res.blob())
-        .then(async (blob) => {
-          return {
-            alt: media.ext_alt_text ?? "",
-            file: new File([blob], media.display_url, { type: blob.type }),
-            aspectRatio: {
-              width: media.sizes.large.w,
-              height: media.sizes.large.h,
-            },
-            objectUrl: URL.createObjectURL(blob),
-          };
-        });
-    });
-    Promise.all(imagesPromise ?? []).then((images) => {
-      postFormMethods.setValue("images", images);
-    });
-  }, [tweet]);
 
   return (
     <>
--- apps/tweet-to-sky/src/components/PostView/PostView.tsx
+++ apps/tweet-to-sky/src/components/PostView/PostView.tsx
@@ -1,7 +1,5 @@
-import { RichText } from "@atproto/api";
 import { Box, Flex, IconButton, Spacer } from "@chakra-ui/react";
-import { FC, useMemo } from "react";
-import { useFieldArray, useFormContext } from "react-hook-form";
+import { FC } from "react";
 import { IoImageOutline } from "react-icons/io5";
 
 import { useAgent } from "@/components/AgentProvider.tsx";
@@ -9,11 +7,9 @@ import CharProgress from "@/components/PostView/CharProgress.tsx";
 import { Gallery } from "@/components/PostView/Gallery.tsx";
 import TextEditor from "@/components/PostView/TextEditor.tsx";
 import { Avatar } from "@/components/ui/avatar.tsx";
-import { getImageFileAspectRatio, selectLocalImages } from "@/helpers/utils.ts";
 
 import { BORDER_COLOR } from "@/constants.ts";
-
-const MAX_IMAGE_LENGTH = 4;
+import { usePostFormContext } from "@/hooks/usePostForm.ts";
 
 export interface PostForm {
   text: string;
@@ -30,53 +26,16 @@ interface Props {}
 const PostView: FC<Props> = ({}) => {
   const { profile } = useAgent();
   const {
-    register,
-    watch,
-    getValues,
-    control,
-    formState: { isSubmitting },
-  } = useFormContext<PostForm>();
-  const {
-    fields: images,
-    replace: replaceImage,
-    remove: removeImage,
-  } = useFieldArray({
-    control,
-    name: "images",
-  });
-
-  const textValue = watch("text");
-  const hasMaxImages = MAX_IMAGE_LENGTH <= images.length;
-  const graphemeLength = useMemo(
-    () => new RichText({ text: textValue }).graphemeLength,
-    [textValue],
-  );
-
-  const onAddImage = async () => {
-    const addedImageFiles = await selectLocalImages();
-    if (!addedImageFiles) {
-      return;
-    }
-
-    const addedImagesPromise = addedImageFiles.map(async (file) => {
-      return {
-        alt: "", // TODO
-        file,
-        aspectRatio: await getImageFileAspectRatio(file),
-        objectUrl: URL.createObjectURL(file),
-      };
-    });
-    const addedImages = await Promise.all(addedImagesPromise);
-
-    const current = getValues("images");
-    replaceImage([...current, ...addedImages].slice(0, 4));
-  };
-
-  const onRemoveImage = (index: number) => {
-    const target = getValues("images")[index];
-    URL.revokeObjectURL(target.objectUrl);
-    removeImage(index);
-  };
+    hasMaxImages,
+    graphemeLength,
+    form: {
+      register,
+      formState: { isSubmitting },
+    },
+    imagesField: { fields: images },
+    onAddImage,
+    onRemoveImage,
+  } = usePostFormContext();
 
   return (
     <>
