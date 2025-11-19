d67f5e7740fe07cb6d7b15fc8b15196c2d605968
TatsuyaYamamoto <t28.is.tatsuya@gmail.com>
2025-11-20 00:53:54 +0900

    chore: add basePath

--- apps/diff/vite.config.ts
+++ apps/diff/vite.config.ts
@@ -1,9 +1,13 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
+
+const basePath = "/diff";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: basePath,
   plugins: [
     react({
       babel: {
@@ -11,5 +15,11 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
+    createHtmlPlugin({
+      inject: { ejsOptions: { views: ["../../packages/shared/ejs"] } },
+    }),
   ],
+  build: {
+    outDir: `../../dist${basePath}`,
+  },
 });
